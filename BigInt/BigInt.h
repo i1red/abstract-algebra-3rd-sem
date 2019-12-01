9e98edaa0a9419b0f04c7d06023d372e5c58f6e3
i1red <ivankras67@gmail.com>
2019-12-01 13:58:08 +0200

    Added new structure BigInt

---
+++ BigInt/BigInt.h
@@ -0,0 +1,62 @@
+#ifndef ABSTRACT_ALGEBRA_BIGINT_H
+#define ABSTRACT_ALGEBRA_BIGINT_H
+
+#include <ostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <utility>
+#include <stdexcept>
+#include <sstream>
+#include <iostream>
+
+class BigInt {
+    std::vector<int> nums;
+    bool nonNegative;
+    static const int BASE;
+    
+public:
+    explicit BigInt(const std::string& init = "0");
+    BigInt(long);
+
+    static const BigInt ZERO;
+    static const BigInt ONE;
+
+    bool operator==(const BigInt&) const;
+    bool operator!=(const BigInt&) const;
+    bool operator>(const BigInt&) const;
+    bool operator<(const BigInt&) const;
+    bool operator>=(const BigInt&) const;
+    bool operator<=(const BigInt&) const;
+
+    bool operator==(const long&) const;
+    bool operator!=(const long&) const;
+    bool operator>(const long&) const;
+    bool operator<(const long&) const;
+    bool operator>=(const long&) const;
+    bool operator<=(const long&) const;
+
+    BigInt operator+(const BigInt&) const;
+    BigInt operator-(const BigInt&) const;
+    BigInt operator*(const BigInt&) const;
+    BigInt operator/(const BigInt&) const;
+    BigInt operator%(const BigInt&) const;
+    BigInt operator-() const;
+    BigInt mod(const BigInt&) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const BigInt& bigInt);
+
+private:
+    BigInt add(const BigInt&) const;
+    BigInt subtract(const BigInt&) const;
+    BigInt multiplication(const BigInt&) const;
+    std::pair<BigInt, BigInt> absDivide(const BigInt&) const;
+    std::pair<BigInt, BigInt> absFillToFit(const BigInt &other) const;
+    BigInt eqPositive(const BigInt&) const;
+    int absCompareTo(const BigInt&) const;
+    std::string absToString() const;
+    void removeLeadingZeros();
+};
+
+#endif //ABSTRACT_ALGEBRA_BIGINT_H
