57c66f77fb838a1e374876daabb91817d08a6cf7
Гестя <54850245+Gestya@users.noreply.github.com>
2019-12-04 19:46:21 +0200

    Add files via upload

---
+++ Carmichael function/Carmichael function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int function(int n)
+{
+	int k = 1, x, a, b, t, d = 1;
+
+
+	for (d = x = 0;++x < n;d = a < 2 & t>1 ? k++ : d)
+	{
+		for (a = x, b = n;t = b;a = t)
+			b = a % b;
+		for (t = 1, b = k;b--;t = t * x%n)
+			;
+	}
+	return k;
+}
+
+ int main() 
+{
+	 for(int i = 1; i < 37; i++)
+		std::cout << i << "= "<< function(i) << std::endl;
+
+};
